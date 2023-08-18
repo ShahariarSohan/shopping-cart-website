a151f32cbf602e296485d66031df00e060f5fb46
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-18 19:31:29 +0600

    shopping cart completed

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+let total =0;
 function shoppingCart(data){
     const text = data.parentNode.childNodes[1].innerText
     const calculate = document.getElementById('calculation')
     const p = document.createElement('p')
-    p.innerText=text;
+    p.innerText=text
     calculate.appendChild(p)
+    const priceText = data.parentNode.childNodes[3].innerText.split(' ')[1]
+    const price = parseFloat(priceText);
+    total = total + price;
+    const setPrice = calculate.childNodes[5].childNodes[1];
+    setPrice.innerText =total;
 }
